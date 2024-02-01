3ee2a43b141458ee14f5526f47c4edc3bd7a36b0
Mariam Senzia <mariamsenzia@gmail.com>
2024-02-01 11:30:19 +0300

    modified files make the app interactive

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart , setCart] = useState(false)
+
+  function handleCart () {
+    setCart((inCart) => !inCart)
+  }
+
   return (
-    <li className="">
+    <li className= {inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>{inCart ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
